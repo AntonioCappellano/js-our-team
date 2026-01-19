f978392d106e757fd61206da75fc9ff4e86ba301
AntonioCappellano <antoniocappellano97@gmail.com>
2026-01-19 15:06:33 +0100

    add card members with a for

--- script.js
+++ script.js
@@ -3,36 +3,54 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "img/male1.png",
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "img/female1.png",
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "img/male2.png",
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "img/female2.png",
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "img/male3.png",
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
-  }
+    img: "img/female3.png",
+  },
 ];
+
+const teamContainer = document.getElementById("team-container");
+let cards = "";
+for (const member of teamMembers) {
+  cards += `
+  <div class="team-card">
+        <div class="team-img">
+          <img src="./${member.img}" />
+        </div>
+        <div class="card-text">
+          <h4>${member.name}</h4>
+          <p>${member.role}</p>
+          <p>${member.email}</p>
+        </div>
+      </div>
+  `;
+}
+teamContainer.innerHTML = cards;
